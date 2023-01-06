05b3d28783b8c7c7f5e650cf6d1b95c9397790b0
Mark Huang <ylhuang947@gmail.com>
2023-01-06 22:02:24 +0800

    vim tmux navigator

--- .config/nvim/lua/start-plugins.lua
+++ .config/nvim/lua/start-plugins.lua
@@ -11,6 +11,8 @@ require('packer').startup(function(use)
   -- Package manager
   use { 'wbthomason/packer.nvim' }
 
+  -- use { 'christoomey/vim-tmux-navigator' }
+
   use { -- LSP Configuration & Plugins
     'neovim/nvim-lspconfig',
     requires = {
