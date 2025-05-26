3553582abae7d6af3b37943e8a557d96b232cb1d
Mark Huang <ylhuang947@gmail.com>
2025-05-26 21:55:16 +0800

    add timeout

--- .config/nvim/lua/plugins/formatters.lua
+++ .config/nvim/lua/plugins/formatters.lua
@@ -31,13 +31,10 @@ return {
 			lsp_format = "fallback",
 		},
 		-- Set up format-on-save
-		format_on_save = function(bufnr)
-			-- Disable with a global or buffer-local variable
-			if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-				return
-			end
-			return { timeout_ms = 500, lsp_format = "fallback" }
-		end, -- Customize formatters
+		format_on_save = {
+			timeout_ms = 2500,
+			lsp_fallback = true,
+		},
 		formatters = {
 			shfmt = {
 				prepend_args = { "-i", "2" },
